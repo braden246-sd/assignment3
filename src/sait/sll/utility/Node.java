53f09d49098f7e841a31fdef771fa3760c6ec7e2
vandermoose1 <ianvanderkley@gmail.com>
2026-03-21 17:50:52 -0600

    Update Node.java
    defining serialversion

--- src/sait/sll/utility/Node.java
+++ src/sait/sll/utility/Node.java
@@ -2,6 +2,7 @@ package sait.sll.utility;
 import java.io.Serializable;
 
 public class Node implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	private Object data;
 	private Node next;
